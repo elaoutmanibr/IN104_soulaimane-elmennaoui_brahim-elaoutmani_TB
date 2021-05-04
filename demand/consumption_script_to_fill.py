79a164d4a92d6960c18bd2a13273c6af5c089d8c
brahim <brahimelaoutmani@gmail.com>
2021-05-04 15:22:42 +0200

    demand code completed and run successfully

--- demand/consumption_script_to_fill.py
+++ demand/consumption_script_to_fill.py
@@ -13,73 +13,91 @@ def set_wd(wd):
 
 #This function imports a csv file and has the option to plot its value columns as a function of the first column
 def import_csv(f_name = "DE.csv", delimiter = ";", plot = True):
-    f = pd.read_csv(f_name,delimiter)
-    f = f >> mutate(Date = pd.to_datetime(f['Date (CET)'])) # we create another column named "Date"
+    df = pd.read_csv(f_name,sep=delimiter)
+    df = df >> mutate(Date = pd.to_datetime(df['Date (CET)'])) # we create another column named "Date"
     if plot: 
-        fig, axes = plt.subplots(nrows=3, ncols=1)
-        f.plot(x='Date', y='LDZ', ax=axes[0])
-        f.plot(x='Date', y='Actual', ax=axes[1])
-        f.plot(x='Date', y='Normal', ax=axes[2])
+        dfig, axes = plt.subplots(nrows=3, ncols=1)
+        df.plot(x='Date', y='LDZ', ax=axes[0])
+        df.plot(x='Date', y='Actual', ax=axes[1])
+        df.plot(x='Date', y='Normal', ax=axes[2])
         plt.show()
     return 
 
 #This function creates a scatter plot given a DataFrame and an x and y column
-def scatter_plot(dataframe = "conso", x = "Actual", y = "LDZ", col = "red"):
+def scatter_plot(dataframe = "df", x = "Actual", y = "LDZ", col = "red"):
+    dataframe.plot.scatter(x=x,y=y,c=col)
+    #plt.show() 
 
 #This function is the sigmoid function for gas consumption as a function of temperature
 def h(t, a, b, c, d):
     return(d+a/(1+(b/(t-40))**c))
 
 #The following function takes sigmoid parameters values and a temperature vector as input and plots the sigmoid, can compare it with the actual values
-def consumption_sigmoid(t, real_conso, a = 900, b = -35, c = 6, d = 300, plot = True):
-  
+def consumption_sigmoid(t, real_conso, a = 500, b = -25, c = 2, d = 100, plot = True):
+    h_hat = np.empty(len(t))
     h_hat = h(t, a, b, c, d)
 
     if plot:
-        plt.plot()
+        plt.plot(t,h_hat,'r')
         #if real_conso is not None you plot it as well
         if not isinstance(real_conso, type(None)):
-            plt.plot()
+            plt.plot(t,real_conso,'b.')
             if(len(t) != len(real_conso)):
                 print("Difference in length between Temperature and Real Consumption vectors")
-            # add title and legend and show plot
+        # add title and legend and show plot
+        plt.xlabel("Temperature")
+        plt.legend(handles=[red_patch,blue_patch],labels=["","Real Consumption"])
+        plt.show()
+        
     return h_hat
 
 #The following function gets the fit metrics list between 2 sigmoids
 def get_fit_metrics(h_hat, real_conso):
     if(len(h_hat) != len(real_conso)):
-        print("Difference in length between Fit and Real Consumption vectors")
+        print("Difference in length between Fit and Real Consumption vectors:\t",abs(len(h_hat)-len(real_conso)))
+        return
     else:
-        
-    return []
+        print("Same length ! ")
+        corr, _ = pearsonr(h_hat, real_conso)
+        rmse = np.sqrt(mean_squared_error(h_hat,real_conso))
+        nrmse = rmse/(np.max(real_conso) - np.min(real_conso))
+        anrmse = rmse/np.mean(real_conso)
+        return [corr,rmse,nrmse,anrmse]
 
-#The following class is the cosumption class it takes sigmoid parameters as well as a temperature as input
+#The following class is the consumption class it takes sigmoid parameters as well as a temperature as input
 class consumption:
     #Initialize class
     def __init__(self, a, b, c, d):
-        
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
 
     #calculate the consumption given a temperature
     def get_consumption(self, temperature):
-        
+        return (self.d+self.a/(1+(self.b/(temperature-40))**self.c))
+
 
     #get the sigmoid considering a temperature between -40 and 39, use the function consumption_sigmoid above
     def sigmoid(self, p):
-
+        l = len(p)
+        t = np.linspace(-40,39,l) # 39 is included !
+        return consumption_sigmoid(t, p)
+    
     #This is what the class print if you use the print function on it
     def __str__(self):
-        
+        t = "consumption coeff initialized"
         return t
 
 #The following class optimizes the parameters of the sigmoid and returns an object of class consumption
 class optimize_sigmoid:
-    #Initialize guess values that are common to all instances of the clasee
-    __guess_a, __guess_b, __guess_c, __guess_d
+    #Initialize guess values that are common to all instances of the classe
+    __guess_a, __guess_b, __guess_c, __guess_d = 500, -25, 2, 100
 
     def __init__(self, f):
         if isinstance(f, pd.DataFrame):
             if 'Actual' and 'LDZ' in f.columns:
-                
+                self.__f = f
             else:
                 print("Class not initialized since f does not contain Actual and LDZ column names")
         else:
@@ -88,15 +106,16 @@ class optimize_sigmoid:
     #optimize and return metrics use functions h, consumption_sigmoid defined above as well as get_fit_metrics
     def optimize(self):
         if self.__f is not None:
-            self.__coef, self.__cov = curve_fit(
-                h,
-
-                )
+            t = self.__f['Actual'].values
+            real = self.__f['LDZ'].values 
+            self.__coef, self.__cov = curve_fit(h,t,real,[__guess_a, __guess_b, __guess_c, __guess_d])
             
-            s = consumption_sigmoid(
-
-                plot = True
-                )
+            s = consumption_sigmoid(t, real,
+             a=self.__coef[0], 
+             b=self.__coef[1],
+             c=self.__coef[2],
+             d=self.__coef[3],
+             plot = True)
             
             self.__corr, self.__rmse, self.__nrmse, self.__anrmse = get_fit_metrics(s, self.__f['LDZ'])
         else:
@@ -104,22 +123,22 @@ class optimize_sigmoid:
 
     #this function returns the fit metrics calculated above
     def fit_metrics(self):
-        if  is not None:
-            return 
+        if  self.__coef is not None:
+            return [self.__corr, self.__rmse, self.__nrmse, self.__anrmse]
         else:
             print("optimize method is not yet run")
 
     #This function creates the class consumption
     def create_consumption(self):
-        if  is not None:
-            return 
+        if  self.__coef is not None:
+            return consumption(self.__coef[0],self.__coef[1],self.__coef[2],self.__coef[3])
         else:
             print("optimize method is not yet run")
 
     #This is what the class print if you use the print function on it
     def __str__(self):
-        if is not None:
-
+        if self.__coef is not None:
+            t = "already optimized"
         else:
             t = "optimize method is not yet run"
         return t
@@ -128,7 +147,7 @@ class optimize_sigmoid:
 if __name__ == '__main__':
 
     #set working directory
-    set_wd()
+    #set_wd()
 
     #1) import consumption data and plot it
     conso = import_csv()
